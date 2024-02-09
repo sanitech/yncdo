541bd212f4430e0b1a07da0fa73f4ba6350885e5
sanitech <sanitech47@gmail.com>
2024-02-09 19:48:29 +0300

    remove .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const env= require("dotenv").config();
+// const env= require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port =3000;
 
 app.use(express.json()); // to accept json data(path.join(__dirname, "public")));
 
