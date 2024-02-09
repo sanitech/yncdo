b568998a1100cdcd71c1d7c049f47df33571cd6e
sanitech <sanitech47@gmail.com>
2024-02-09 17:22:46 +0300

    new commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const env= require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require("path");
+// const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/',(req, res) => {
     res.send('working');
